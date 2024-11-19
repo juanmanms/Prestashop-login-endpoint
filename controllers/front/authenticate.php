8e0b833473cd660062c82c18e171c92c980ec4d6
Juanmanms <juanmanms@gmail.com>
2024-11-19 16:36:21 +0100

    quitando hook

--- controllers/front/authenticate.php
+++ controllers/front/authenticate.php
@@ -9,6 +9,15 @@ class AuthenticationApiAuthenticateModuleFrontController extends ModuleFrontCont
         parent::initContent();
 
         header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+        // Manejar la solicitud OPTIONS (preflight)
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            http_response_code(200);
+            exit();
+        }
 
         $input = json_decode(file_get_contents('php://input'), true);
         $email = isset($input['email']) ? $input['email'] : null;
@@ -29,15 +38,13 @@ class AuthenticationApiAuthenticateModuleFrontController extends ModuleFrontCont
 
         // Generar token y guardarlo
         $token = bin2hex(random_bytes(32));
-        $expiryDate = date('Y-m-d H:i:s', strtotime('+12 hour'));
+        $expiryDate = date('Y-m-d H:i:s', strtotime('+1 hour'));
 
         Db::getInstance()->insert('authentication_token', [
             'id_employee' => $employee->id,
             'token' => $token,
             'expiry_date' => $expiryDate,
         ]);
-
-        http_response_code(200);
 
         echo json_encode(['token' => $token, 'expiry_date' => $expiryDate,  'Profile employed' => $employee->id_profile]);
     }
